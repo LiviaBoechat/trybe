37a57a70e315245965f79d440ebe2e521293f4df
Livia Boechat <limaboechatti@gmail.com>
2022-10-22 22:06:57 -0300

    exjsdia1

---
+++ fundamentos/secao04-dia02/script.js
@@ -0,0 +1,31 @@
+// ex01: Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) { //index ++ OU index += 1 OU index = index + 1 
+    console.log(numbers[index]);
+}
+*/
+
+// ex02: Some todos os valores contidos no array e imprima o resultado;
+
+/*
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0; //zero é valor inicial que vai ser somado aos valores que estão nos índices do array
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index] //let soma vai somar o que já tem nela, mais o novo valor do índice
+}
+
+console.log(soma); 
+*/
+
+// ex03: Calcule e imprima a média aritmética dos valores contidos no array; (A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.)
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    
+}
